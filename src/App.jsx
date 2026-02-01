f4d5fbde833704ee0772480a388eab581abf1288
sherry <chenyeeshen@gmail.com>
2026-02-01 10:42:39 +0800

    寫到一半

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+
+import "./assets/style.css";
+
+// API 設定
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData((preData) => ({
+      ...preData,
+      [name]: value,
+    }))
+  }
+
+  // 登入狀態管理(控制顯示登入或產品頁）
+  const [isAuth, setIsAuth] = useState(false);
+  // 產品資料狀態
+  const [products, setProducts] = useState([]);
+  // 目前選中的產品
+  const [tempProduct, setTempProduct] = useState(null);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {
+      !isAuth ? (<div className="container login">
+      <h1>請先登入</h1>
+      <form className="form-floating">
+        <div className="form-floating mb-3">
+          <input type="email" className="form-control" name="username" placeholder="name@example.com" value={formData.username} onChange={(e) => handleInputChange(e)} />
+            <label htmlFor="username">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input type="password" className="form-control" name="password" placeholder="Password" value={formData.password} onChange={(e) => handleInputChange(e)} />
+            <label htmlFor="password">Password</label>
+        </div>
+        <button type='submit' className='btn btn-primary w-100 mt-2'>登入</button>
+      </form>
+    </div>) : (
+      <div className="container">已登入</div>
+    )
+    }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
